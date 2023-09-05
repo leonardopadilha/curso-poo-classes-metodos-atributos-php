b7bdd85b9a4d046a42aa12be5c59028b90a98c80
leonardopadilha <leonardopadilha28@gmail.com>
2023-09-05 12:52:34 -0300

    Método construtor

--- banco.php
+++ banco.php
@@ -2,6 +2,22 @@
 
 require_once 'src/Conta.php';
 
-$primeiraConta = new Conta();
-$primeiraConta->depositar(100);
-var_dump($primeiraConta);
+$primeiraConta = new Conta('Teste teste da silva', '123.456.789-10');
+$primeiraConta->deposita(100);
+$primeiraConta->saca(10);
+
+echo $primeiraConta->recuperaSaldo();
+echo PHP_EOL;
+echo $primeiraConta->recuperaCPFTitular();
+echo PHP_EOL;
+echo $primeiraConta->recuperaNomeTitular();
+
+$segundaConta = new Conta('Silva teste do teste', '123.456.789.11');
+$primeiraConta->transfere(40, $segundaConta);
+
+echo PHP_EOL;
+echo $segundaConta->recuperaSaldo();
+echo PHP_EOL;
+echo $primeiraConta->recuperaSaldo();
+
+//var_dump($primeiraConta);
--- src/Conta.php
+++ src/Conta.php
@@ -3,9 +3,16 @@
 class Conta {
     private string $cpfTitular;
     private string $nomeTitular;
-    private float $saldo = 0;
+    private float $saldo;
 
-    public function sacar(float $valorASacar)
+    public function __construct(string $nomeTitular, string $cpfTitular)
+    {
+        $this->nomeTitular = $nomeTitular;
+        $this->cpfTitular = $cpfTitular;
+        $this->saldo = 0;     
+    }
+
+    public function saca(float $valorASacar)
     {
         if ($valorASacar > $this->saldo) {
             echo "Saldo indisponível";
@@ -15,7 +22,7 @@ class Conta {
         $this->saldo -= $valorASacar;
     }
 
-    public function depositar(float $valorADepositar): void
+    public function deposita(float $valorADepositar): void
     {
         if ($valorADepositar < 0) {
             echo "Valor precisa ser positivo";
@@ -25,14 +32,29 @@ class Conta {
         $this->saldo += $valorADepositar;
     }
 
-    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
     {
         if ($valorATransferir > $this->saldo) {
             echo "Saldo indisponível";
             return;
         }
         
-        $this->sacar($valorATransferir);
-        $contaDestino->depositar($valorATransferir);
+        $this->saca($valorATransferir);
+        $contaDestino->deposita($valorATransferir);
+    }
+
+    public function recuperaSaldo() : float
+    {
+        return $this->saldo;
+    }
+
+    public function recuperaCPFTitular() : string
+    {
+        return $this->cpfTitular;
+    }
+    
+    public function recuperaNomeTitular() : string
+    {
+        return $this->nomeTitular;
     }
 }
